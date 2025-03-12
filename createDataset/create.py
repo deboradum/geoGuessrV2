45da9b26b4efed96ef14aca5ed21ba4ce89184a9
deboradum <pvwpvw@protonmail.com>
2025-03-12 20:53:46 +0100

    dataset creation works

--- createDataset/create.py
+++ createDataset/create.py
@@ -5,23 +5,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import random
+import string
 import requests
 import os
 import time
 from PIL import Image
 from io import BytesIO
+from dotenv import load_dotenv
+from urllib.parse import urlparse, parse_qs
 
-# Set up Selenium WebDriver
+load_dotenv()
+
 options = webdriver.ChromeOptions()
-# options.add_argument(
-#     "--headless"
-# )  # Run headless (remove if you want to see the browser)
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920x1080")
 options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
 
-service = Service("chromedriver")  # Adjust path if necessary
+service = Service("chromedriver")
 browser = webdriver.Chrome(service=service, options=options)
+
+known_coords = {}
+
+def random_string(length=10):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
 def get_image_urls(logs):
@@ -43,64 +50,32 @@ def get_image_urls(logs):
 
                 if is_image:
                     image_urls.add(url)
-
         except (KeyError, json.JSONDecodeError):
             pass
 
     return image_urls
 
 
-def download_images(urls, folder="images"):
-    """Downloads images from given URLs."""
-    os.makedirs(folder, exist_ok=True)
-
+def download_images(urls, location_key, directory="images"):
     for i, url in enumerate(urls):
-        print(f"Downloading {i+1}/{len(urls)}: {url}")
+        print(f"Downloading {i+1}/{len(urls)}")
         try:
             response = requests.get(url, stream=True)
-            if response.status_code == 200:
-                try:
-                    img = Image.open(BytesIO(response.content))
-                    width, height = img.size
+            if response.status_code != 200:
+                continue
+            try:
+                img = Image.open(BytesIO(response.content))
+                width, height = img.size
+                if width != 512 and height != 512:
+                    continue
 
-                    if width == 512 and height == 512:
-                        # Extract filename from URL or use a generic one
-                        filename = os.path.basename(url).split("?")[0]
-                        if not any(
-                            filename.lower().endswith(ext)
-                            for ext in [
-                                ".jpg",
-                                ".jpeg",
-                            ]
-                        ):
-                            ext = ".jpg"
-                            filename = f"image_{i}{ext}"
-
-                        with open(os.path.join(folder, filename), "wb") as file:
-                            file.write(response.content)
-
-                except (IOError, OSError) as e:
-                    print(f"Unable to determine image dimensions: {e}")
-
-                # Extract filename from URL or use a generic one
-                filename = os.path.basename(url).split("?")[0]
-                if not any(
-                    filename.lower().endswith(ext)
-                    for ext in [
-                        ".jpg",
-                        ".jpeg",
-                    ]
-                ):
-                    ext = ".jpg"
-                    filename = f"image_{i}{ext}"
-
-                with open(os.path.join(folder, filename), "wb") as file:
-                    for chunk in response.iter_content(1024):
-                        file.write(chunk)
-            else:
-                print(f"Failed to download {url}: HTTP {response.status_code}")
-        except Exception as e:
-            print(f"Failed to download {url}: {e}")
+                filename = f"{location_key}_{i}.jpg"
+                with open(os.path.join(directory, filename), "wb") as file:
+                    file.write(response.content)
+            except (IOError, OSError) as e:
+                print(f"Unable to determine image dimensions: {e}")
+        except Exception as _:
+            print("Failed to download image")
 
 
 def accept_cookies():
@@ -126,10 +101,69 @@ def goto_next_location():
     time.sleep(5)  # Wait for new image to load
 
 
+def get_location():
+    address_element = browser.find_element(By.ID, "address")
+    return address_element.text
+
+
+def get_coords(logs):
+    for entry in reversed(logs):  # Start from the most recent log
+        if "message" in entry:
+            try:
+                log_message = json.loads(entry["message"])
+
+                # Check if 'params' and 'request' are in the message
+                if (
+                    "params" in log_message["message"]
+                    and "request" in log_message["message"]["params"]
+                ):
+                    url = log_message["message"]["params"]["request"]["url"]
+
+                    # Check if the URL matches the Street View URL
+                    if "streetviewpixels-pa.googleapis.com" in url:
+                        parsed_url = urlparse(url)
+                        params = parse_qs(parsed_url.query)
+                        panoidID = params.get("panoid", [None])[0]
+
+                        if panoidID:
+                            print(panoidID)
+                            break
+            except (json.JSONDecodeError, KeyError):
+                continue
+
+    coords = known_coords.get(panoidID)
+    if coords:
+        return coords
+
+    api_key = os.getenv("GOOGLE_API_KEY")
+    url = "https://maps.googleapis.com/maps/api/streetview/metadata"
+    params = {"pano": panoidID, "key": api_key}
+
+    try:
+        response = requests.get(url, params=params)
+        if response.status_code != 200:
+            print(f"Error: Received status code {response.status_code}")
+            raise Exception("Got status code", response.status_code)
+
+        data = response.json()
+        print(data)
+
+        location = data["location"]
+        known_coords[panoidID] = (location["lat"], location["lng"])
+
+        return location["lat"], location["lng"]
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+        raise e
+
 
 if __name__ == "__main__":
+    country = "co"
+    directory = "colombia"
+    os.makedirs(directory, exist_ok=True)
     try:
-        browser.get("https://randomstreetview.com")
+        browser.get(f"https://randomstreetview.com/{country}")
         WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "next")))
 
         dont_download_urls = set()
@@ -139,16 +173,19 @@ if __name__ == "__main__":
 
         accept_cookies()
 
-        for _ in range(10):
+        for _ in range(89):
             goto_next_location()
 
             logs = browser.get_log("performance")
             image_urls = get_image_urls(logs)
+            lat, lng = get_coords(logs)
             to_download = [url for url in image_urls if url not in dont_download_urls]
-            print(
-                f"Downloading {len(to_download)} new images"
-            )
-            download_images(to_download)
+
+            print(f"Downloading {len(to_download)} new images")
+            location_key = random_string()
+            with open(os.path.join(directory, "locations.csv"), "a+") as file:
+                file.write(f"{location_key},{lat},{lng}\n")
+            download_images(to_download, location_key, directory)
             dont_download_urls.update(to_download)
     finally:
         browser.quit()
