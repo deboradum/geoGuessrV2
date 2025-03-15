75b8b435a9b2bebd8f3c7c3bc8e4727ffc118d88
deboradum <pvwpvw@protonmail.com>
2025-03-15 10:06:02 +0100

    dataset distribution plot

--- createDataset/visualize.py
+++ createDataset/visualize.py
@@ -2,6 +2,7 @@ import os
 import re
 import folium
 import pandas as pd
+import matplotlib.pyplot as plt
 from folium.plugins import HeatMap
 
 
@@ -33,11 +34,48 @@ def get_all_coords(dataset_dir):
     return coords
 
 
-def visualize(coordinates):
+def get_country_data(dataset_dir):
+    country_data = []
+    for country in os.listdir(dataset_dir):
+        if not os.path.isdir(f"{dataset_dir}/{country}"):
+            continue
+
+        num_images = 0
+        for img_path in os.listdir(f"{dataset_dir}/{country}/"):
+            match = re.match(r"^(.*?)_\d+\.jpg$", img_path)
+            if not match:
+                continue
+
+            num_images += 1
+
+        country_data.append((country, num_images))
+
+    return country_data
+
+
+def plot_heatmap(coordinates):
     m = folium.Map(location=[20, 0], zoom_start=2)
     HeatMap(coordinates, radius=15).add_to(m)
     m.save("heatmap.html")
 
+
+def plot_country_distribution(country_data):
+    # Sort data from least to most images
+    country_data.sort(key=lambda x: x[1])
+    countries, num_images = zip(*country_data)
+
+    plt.figure(figsize=(12, 6))
+    plt.bar(countries, num_images, color="skyblue")
+    plt.xlabel("Country")
+    plt.ylabel("Number of Images")
+    plt.title("Number of Images per Country")
+    plt.xticks(rotation=90)
+    plt.show()
+
+
 if __name__ == "__main__":
-    coords = get_all_coords("dataset/")
-    visualize(coords)
+    # coords = get_all_coords("dataset/")
+    # plot_heatmap(coords)
+
+    country_data = get_country_data("dataset/")
+    plot_country_distribution(country_data)
