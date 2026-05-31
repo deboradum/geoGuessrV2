73a9ee179e5e941c506c8b505414e57ea4d178cc
deboradum <pvwpvw@protonmail.com>
2026-05-31 17:58:59 +0200

    scaled dist loss

--- utils.py
+++ utils.py
@@ -17,6 +17,7 @@ class TrainConfig:
     net_name: str = "convnext-tiny"
     num_experts: int = 8
     router_k: int = 2
+    dist_loss_weight: float = 1.0
     s2_loss_weight: float = 0.5
     load_balance_loss_weight: float = 0.01
     embedding_dim: int = 512
